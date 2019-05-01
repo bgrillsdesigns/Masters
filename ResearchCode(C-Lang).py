93c64782e2e1bde38c8636471fab222b75d4cf74
bgrillsdesigns <bgrillsdesigns@gmail.com>
2019-04-30 22:00:18 -0400

    small fixes to improve alpha

--- ResearchCode(C-Lang).py
+++ ResearchCode(C-Lang).py
@@ -4,11 +4,8 @@
 #of all commented out codes
 
 import sys
-
-#infile = open(sys.argv[-1], "r")
-inFileR = open("TestingProgram.cpp", "r")
-oFile = open("CleanedProgram.cpp", "w")
-qFile = open("quarentine.md", "w")
+import glob
+import os
 
 def CheckForComments(inFileR, qFile, oFile) :
     lowLine = 0
@@ -20,6 +17,8 @@ def CheckForComments(inFileR, qFile, oFile) :
     for line in inFileR :
         lowLine = lowLine + 1
         for i in range(0, line.__len__()) :
+            if line[i:i+1] == ";" :
+                break
             if line[i:i+2] == "//" :
                 for j in range(0, line.__len__()) :
                     if line[j:j+1] == ";" or line[i:i+1] == ")" :
@@ -50,6 +49,9 @@ def CommentBlockCheck(lowLine, inFileR, line) :
                 containsComments = True
             if containsComments == True and line[i:i+2] == "*/" :
                 tempHigh = tempLow
+                semicolon = tempStringArray.find(";")
+                if(semicolon == -1) :
+                    return 0, ""
                 return tempHigh + lowLine, tempStringArray             
             if containsComments == False and line[i:i+2] == "*/" :
                 return 0, ""
@@ -72,29 +74,46 @@ def BuildClean(inFileR, oFile, lowLineContainer, highLineContainer) :
     tempLine = 1
     i = 0
     blockTracer = False
+    
     for line in inFileR :
-
-        if (int(lowLineContainer[i]) == tempLine and lowLineContainer[i] == highLineContainer[i]) :
-            oFile.write("//commented out code was ommited here \n")
-            i = i + 1
-            if (i == highLineContainer.__len__()) :
-                    i = highLineContainer.__len__() - 1
-        elif (int(lowLineContainer[i]) == tempLine and lowLineContainer[i] < highLineContainer[i]) :
-            oFile.write("//commented out code was ommited here \n")
-            blockTracer = True
-        elif (blockTracer == True and tempLine <= int(highLineContainer[i])) :
-            oFile.write("//commented out code was ommited here \n")
-            if(tempLine == int(highLineContainer[i])) :
-                blockTracer = False
+        if (lowLineContainer.__len__() > 0) :
+            if (int(lowLineContainer[i]) == tempLine and lowLineContainer[i] == highLineContainer[i]) :
+                oFile.write("//commented out code was ommited here \n")
                 i = i + 1
                 if (i == highLineContainer.__len__()) :
                     i = highLineContainer.__len__() - 1
+            elif (int(lowLineContainer[i]) == tempLine and lowLineContainer[i] < highLineContainer[i]) :
+                oFile.write("//commented out code was ommited here \n")
+                blockTracer = True
+            elif (blockTracer == True and tempLine <= int(highLineContainer[i])) :
+                oFile.write("//commented out code was ommited here \n")
+                if(tempLine == int(highLineContainer[i])) :
+                    blockTracer = False
+                    i = i + 1
+                    if (i == highLineContainer.__len__()) :
+                        i = highLineContainer.__len__() - 1
+            else :
+                oFile.write("%s" %(line))
         else :
             oFile.write("%s" %(line))
         tempLine = tempLine + 1
 
-CheckForComments(inFileR, qFile, oFile)
+cp = "CleanedProgram.cs"
+quar = "quarentine.md"
 
-inFileR.close()
-oFile.close()
-qFile.close()
+path = sys.argv[-1]
+for fileName in glob.glob(os.path.join(path, '*.cs')) :
+    #fileName = fileName.split(path)
+    #fileName = fileName[1][1:]
+    inFileR = open(fileName, "r")
+    fileCP = "%s %s" % (fileName[0:-3], cp)
+    fileQuar = "%s %s" % (fileName[0:-3], quar)
+    oFile = open(fileCP, "w")
+    qFile = open(fileQuar, "w")
+
+    CheckForComments(inFileR, qFile, oFile)
+
+    inFileR.close()
+    oFile.close()
+    qFile.close()
+
